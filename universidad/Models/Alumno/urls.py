00b9f27a9ec509310d9b7df5fb1b50a137d5315c
Alberto Martinez <alberto1230@gmail.com>
2026-03-05 12:46:09 -0600

    Creacion de las rutas en urls.py listas y configuradas

--- universidad/Models/Alumno/urls.py
+++ universidad/Models/Alumno/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'alumno'
 
 urlpatterns = [
-    path('', views.alumno_list, name='list')
+    path('', views.alumno_list, name='list'),
+    path('cursos/', views.curso_list, name='cursos'),
+    path('notas/', views.nota_list, name='notas'),
+    path('catedraticos/', views.catedratico_list, name='catedraticos'),
+    path('asignaciones/', views.asignacion_list, name='asignaciones'),
+    path('inscripciones/', views.inscripcion_list, name='inscripciones'),
 ]
